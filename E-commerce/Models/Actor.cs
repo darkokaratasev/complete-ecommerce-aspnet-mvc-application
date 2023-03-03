604ed92f0cc2468d6ca65a54b081bba857cc8aed
darkokaratasev <karatasevdarko@gmail.com>
2023-03-03 21:30:25 +0100

    first

---
+++ E-commerce/Models/Actor.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerce.Models
+{
+    public class Actor
+    {
+        [Key]
+
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+    }
+}
